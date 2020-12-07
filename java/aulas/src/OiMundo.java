699d0138403258b58db412582ce49fea2f8254dc
barbisliboni <niinaliboni@gmail.com>
2020-12-07 10:07:54 -0300

    Primeiro programa em java

---
+++ java/aulas/src/OiMundo.java
@@ -0,0 +1,12 @@
+
+public class OiMundo {
+	
+	public static void main (String[] args) {
+		
+		System.out.println("Hello World! :D"); 
+		// TODO FINAL DE COMANDO DEVO USAR PONTO E VIRGULA
+		
+		
+	}
+
+}
